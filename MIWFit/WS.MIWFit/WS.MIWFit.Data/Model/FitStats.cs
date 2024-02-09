95167c0e21bf7f7372d80449435c1f3502538200
uo271588 <uo271588@uniovi.es>
2024-02-09 20:43:21 +0100

    Cambios Modelo

--- MIWFit/WS.MIWFit/WS.MIWFit.Data/Model/FitStats.cs
+++ MIWFit/WS.MIWFit/WS.MIWFit.Data/Model/FitStats.cs
@@ -1,14 +1,12 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace WS.MIWFit.Data.Model
+﻿namespace WS.MIWFit.Data.Model
 {
     public class FitStats
     {
         public int Id { get; set; }
-        public long Weigth { get; set; }
-        public long Heigth { get; set; }
+        public long Weight { get; set; }
+        public long Height { get; set; }
         public long IMC { get; set; }
         public string Activity { get; set; } = string.Empty;
-        
+        public User User { get; set; } = null;
     }
 }
--- MIWFit/WS.MIWFit/WS.MIWFit.Data/Model/User.cs
+++ MIWFit/WS.MIWFit/WS.MIWFit.Data/Model/User.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Mail { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public ICollection<FitStats> FitStats { get; set; } = new List<FitStats>();
     }
 }
