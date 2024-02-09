f6398ad5674366f4d04a66e19f2439cef39fa7d9
UO271588 <uo271588@uniovi.es>
2024-02-09 16:39:25 +0100

    Creación Data

---
+++ MIWFit/WS.MIWFit/WS.MIWFit.Data/Model/FitStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WS.MIWFit.Data.Model
+{
+    public class FitStats
+    {
+        public int Id { get; set; }
+        public long Weigth { get; set; }
+        public long Heigth { get; set; }
+        public long IMC { get; set; }
+        public string Activity { get; set; } = string.Empty;
+        
+    }
+}
