7633c1ad0ce4d791d0a5ebba770da3b99bda0f65
Francisco Fernandez <uo269509@uniovi.es>
2024-02-16 20:43:09 +0100

    API Security hecho

---
+++ MIWFit/WS.MIWFit/WS.MIWFit.Security/Controllers/TokensController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WS.MIWFit.DataWS;
+using WS.Unit10.Task2.Security.Utils;
+
+namespace WS.Unit10.Task2.Security.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+        [HttpGet("{token}")]
+        public ActionResult<String> Get([FromRoute] string token)
+        {
+            try
+            {
+                var securityToken = JsonConvert.DeserializeObject<dynamic>(
+                                                             AESManager.Decrypt(token));
+                if (securityToken == null)
+                {
+                    return NotFound();
+                }
+                else if (DateTime.Parse(
+                    securityToken.ExpirationDate.ToString()) > DateTime.Now)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+            }
+            catch (Exception) { return BadRequest(); }
+        }
+
+        // POST: api/Tokens
+        [HttpPost]
+        public IActionResult Post([FromBody] dynamic tokenRequest)
+        {
+            try
+            {
+                var username = JsonConvert.DeserializeObject<dynamic>(
+                                          tokenRequest.ToString()).Username.ToString();
+                var password = JsonConvert.DeserializeObject<dynamic>(
+                                          tokenRequest.ToString()).Password.ToString();
+                var data = new DataServicesClient();
+                User user = data.GetUserAsync(username).Result;
+                if(user.Username.Length <= 0) 
+                {
+                    return BadRequest();
+                }
+                else 
+                {
+                    if(!user.Username.Equals(username) || !user.Password.Equals(password)) 
+                    {
+                        return BadRequest();
+                    }
+                }
+                var token = AESManager.Encrypt(JsonConvert.SerializeObject(
+                        new { UserName = username, ExpirationDate = DateTime.Now.AddDays(30) }));
+                return CreatedAtAction(
+                                       "Get", new { token = token }, new { Token = token });
+            }
+            catch (Exception) { return BadRequest(); }
+        }
+    }
+}
